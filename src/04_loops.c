f533dc5ed8bfb447a3d9ba6fdf75fb037dedc33c
minur1907 <zuber.minur@ibu.edu.mk>
2026-03-31 06:08:45 +0200

    Update 04_loops.c

--- src/04_loops.c
+++ src/04_loops.c
@@ -27,17 +27,33 @@ int main(void) {
     // TODO 1: keep asking until n > 0
     while (n <= 0) {
         printf("Enter a positive integer n (> 0): ");
+       scanf("%d", &n);
         // TODO: scanf n
         // If scanf fails, clear input safely (optional for beginners)
     }
 
     printf("\n=== Counting ===\n");
     // TODO 2: print 1..n
+   for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
 
     printf("\n=== Sum 1..n ===\n");
+   int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("Sum = %d\n", sum);
     // TODO 3: compute sum
 
     printf("\n=== Factorial n! ===\n");
+   unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial = %llu\n", fact);
     // TODO 4: compute factorial
 
     return 0;
