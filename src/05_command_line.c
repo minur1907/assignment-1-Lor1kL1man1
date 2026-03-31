b6b2e5f1af5e9256c38cf81ffdafa87b4acd99e2
minur1907 <zuber.minur@ibu.edu.mk>
2026-03-31 06:11:41 +0200

    Update 05_command_line.c

--- src/05_command_line.c
+++ src/05_command_line.c
@@ -30,12 +30,40 @@ Hint:
 
 int main(int argc, char *argv[]) {
     // TODO 1: check argc
+  if (argc != 4) {
+        printf("Usage: %s <operation> <num1> <num2>\n", argv[0]);
+        printf("Operations: add, sub, mul, div\n");
+        return 1;
+    }
+
     // argc should be 4
 
     // TODO 2: parse operation and numbers
+  char *op = argv[1];
+    long a = strtol(argv[2], NULL, 10);
+    long b = strtol(argv[3], NULL, 10);
     // char *op = argv[1];
     // long a = strtol(argv[2], NULL, 10);
     // long b = strtol(argv[3], NULL, 10);
+   if (strcmp(op, "add") == 0) {
+        printf("%ld\n", a + b);
+    }
+    else if (strcmp(op, "sub") == 0) {
+        printf("%ld\n", a - b);
+    }
+    else if (strcmp(op, "mul") == 0) {
+        printf("%ld\n", a * b);
+    }
+    else if (strcmp(op, "div") == 0) {
+        if (b == 0) {
+            printf("Cannot divide by zero.\n");
+            return 1;
+        }
+        printf("%ld\n", a / b);
+    }
+    else {
+        printf("Unknown operation\n");
+    }
 
     // TODO 3-5: implement operations
 
