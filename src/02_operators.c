c357f981f78217723994b07fbea981784ea3362f
minur1907 <zuber.minur@ibu.edu.mk>
2026-03-31 06:01:33 +0200

    Update 02_operators.c

--- src/02_operators.c
+++ src/02_operators.c
@@ -25,20 +25,53 @@ int main(void) {
     int a = 0, b = 0;
 
     printf("Enter integer a: ");
+   scanf("%d", &a);
     // TODO: scanf a
     printf("Enter integer b: ");
+   scanf("%d", &b);
     // TODO: scanf b
 
     printf("\n=== Arithmetic ===\n");
+   printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
     // TODO: print +, -, *
 
     // TODO: division + modulo only if b != 0
     // else print "Cannot divide by zero."
+    if (b != 0) {
+        printf("a / b = %d\n", a / b);
+        printf("a %% b = %d\n", a % b);
+    } else {
+        printf("Cannot divide by zero.\n");
+    }
 
     printf("\n=== Comparison ===\n");
+   if (a > b) {
+        printf("a is greater\n");
+    } else if (b > a) {
+        printf("b is greater\n");
+    } else {
+        printf("they are equal\n");
+    }
+
     // TODO: compare and print
 
     printf("\n=== Logical checks ===\n");
+    if (a > 0 && b > 0) {
+        printf("Both numbers are positive.\n");
+    } else {
+        printf("Both numbers are NOT positive.\n");
+    }
+
+    if (a < 0 || b < 0) {
+        printf("At least one number is negative.\n");
+    } else {
+        printf("No negative numbers.\n");
+    }
+
+    return 0;
+}
     // TODO: both positive?
     // TODO: at least one negative?
 
